5689295aaef735edbf06d2fe017054412839a21f
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2023-06-30 17:39:10 +0300

    define the ast for this syntax

---
+++ ast.ts
@@ -0,0 +1,34 @@
+type Expression =
+	| { type: "NilLiteralExpression" }
+	| { type: "CallExpression"; callee: Expression; args: Expression[] }
+	| {
+			type: "BinaryExpression";
+			operator: "LessThan" | "GreaterThan" | "Add";
+			left: Expression;
+			right: Expression;
+	  }
+	| { type: "IdentifierExpression"; name: string }
+	| {
+			type: "NumberLiteralExpression";
+			value: number;
+	  };
+
+type Statement =
+	| { type: "ReturnStatement"; expression: Expression }
+	| {
+			type: "FunctionDeclarationStatement";
+			name: string;
+			body: Statement[];
+			parameters: string[];
+	  }
+	| { type: "SpawnStatement"; spawnee: Expression; args: Expression[] }
+	| { type: "YieldStatement"; expression: Expression }
+	| { type: "PrintStatement"; expression: Expression }
+	| { type: "WhileStatement"; condition: Expression; body: Statement[] }
+	| { type: "ExpressionStatement"; expression: Expression }
+	| {
+			type: "VariableDeclarationStatement";
+			name: string;
+			initializer: Expression;
+	  }
+	| { type: "AssignmentStatement"; name: string; value: Expression };
