00a8e22b1c84eed2c6f22d40ee91f93ef6b9e51b
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2023-08-23 21:27:05 +0300

    let's not show the stacktrace to the user

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 import { VM } from "./vm.ts";
 
 if (import.meta.main) {
-  const source = Deno.readTextFileSync(Deno.args[0]);
-  new VM().run(source);
+  try {
+    const source = Deno.readTextFileSync(Deno.args[0]);
+    new VM().run(source);
+  } catch (error) {
+    console.error(error.message);
+  }
 }
